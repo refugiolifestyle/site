e9e03e4b18d2342c4cf0e926464ebefa4f13985e
Refúgio Lifestyle <luisenriquegomesportugal@hotmail.com>
2024-09-08 17:35:36 -0300

    modal

--- src/components/cadastrar-inscrito/pagamento/modal.tsx
+++ src/components/cadastrar-inscrito/pagamento/modal.tsx
@@ -8,7 +8,7 @@ export function PagamentoModal({ url }: { url?: string }) {
     return <Dialog open={!!url} modal={true}>
         <DialogContent
             closable={false}
-            className="w-full max-w-[900px] h-2/3 my-2 p-0"
+            className="w-full max-w-[900px] h-5/6 my-2 p-0"
             onInteractOutside={(e) => {
                 e.preventDefault();
             }}>
