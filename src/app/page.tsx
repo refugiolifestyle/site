91a8011ee7dbc9c9d3810436682497da16bc8050
Refúgio Lifestyle <luisenriquegomesportugal@hotmail.com>
2025-05-05 16:33:23 -0300

    descrição da hora

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
       alt="Fundo com a logo da Conferência"
       className="absolute bottom-0 left-0 top-0 right-0 object-cover z-[-1] w-full h-screen brightness-50" />
     <h1 className="text-white text-6xl font-thin">Culto Refúgio</h1>
-    <h4 className="text-white text-2xl font-thin">Domingo, 20 hrs</h4>
+    <h4 className="text-white text-2xl font-thin">Domingo, 20h</h4>
     <div className="h-1 w-24 my-4 bg-gradient-to-r from-[#ad1a1c] to-[#830b0c]" />
   </>;
 }
